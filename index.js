5dbddb8449b32f6ead1e78f1c9c059b63c60aeff
Marta Esteves <martizess@gmail.com>
2016-11-20 16:51:47 +0000

    Djakuszhjdjwi

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-var e = document.getElementById('animated');
-/*
-e.addEventListener("animationstart", listener);
-e.addEventListener("animationend", listener);
-e.addEventListener("animationiteration", listener);
-*/
-//e.className = "rotating";
-
-function listener(evt) {
-	//console.log(evt);
-	switch (evt.type) {
-		case "animationstart":
-			e.innerHTML = "1";
-			console.log("Started.");
-			break;
-		case "animationend":
-			e.innerHTML = e.innerHTML + " terminado";
-			console.log("Ended." + e.innerHTML);
-			break;
-		case "animationiteration":
-			e.innerHTML = parseInt(e.innerHTML) + 1;
-			console.log(e.innerHTML);
-			break;
-	}
-
-}
+$("#toggle").click(function(){
+    $("#content").hide();
+});
