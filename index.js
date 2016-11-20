808d99fcf751b3a4d6b99895e692f3d78e833bb7
Marta Esteves <martizess@gmail.com>
2016-11-20 16:26:30 +0000

    Nklç

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var clicked = true;
-$(".one").on('click', function () {
-    if (clicked) {
-        clicked = false;
-        $(".two").css({"top": 0});
-    } else {
-        clicked = true;
-        $(".two").css({"top": "-40px"});
-    }
-});
+var e = document.getElementById('animated');
+/*
+e.addEventListener("animationstart", listener);
+e.addEventListener("animationend", listener);
+e.addEventListener("animationiteration", listener);
+*/
+//e.className = "rotating";
+
+function listener(evt) {
+	//console.log(evt);
+	switch (evt.type) {
+		case "animationstart":
+			e.innerHTML = "1";
+			console.log("Started.");
+			break;
+		case "animationend":
+			e.innerHTML = e.innerHTML + " terminado";
+			console.log("Ended." + e.innerHTML);
+			break;
+		case "animationiteration":
+			e.innerHTML = parseInt(e.innerHTML) + 1;
+			console.log(e.innerHTML);
+			break;
+	}
+
+}
