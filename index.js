589b07470ca6079eb15a68e6e02e86d0f91242bf
Marta Esteves <martizess@gmail.com>
2016-11-20 16:09:29 +0000

    mkl.kgesfdkvz

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var clicked=true;
-$(".one").on('click', function(){
-    if(clicked)
-    {
-        clicked=false;
+var clicked = true;
+$(".one").on('click', function () {
+    if (clicked) {
+        clicked = false;
         $(".two").css({"top": 0});
-    }
-    else
-    {
-        clicked=true;
+    } else {
+        clicked = true;
         $(".two").css({"top": "-40px"});
     }
 });
