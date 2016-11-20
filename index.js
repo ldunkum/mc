a50c796fd101d2567a23bfc7502120716a38e0ea
Marta Esteves <martizess@gmail.com>
2016-11-20 15:59:08 +0000

    Dvd

---
+++ index.js
@@ -0,0 +1,13 @@
+var clicked=true;
+$(".one").on('click', function(){
+    if(clicked)
+    {
+        clicked=false;
+        $(".two").css({"top": 0});
+    }
+    else
+    {
+        clicked=true;
+        $(".two").css({"top": "-40px"});
+    }
+});
